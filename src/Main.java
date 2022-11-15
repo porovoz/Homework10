712c18e968c0183ab27e978b6f5f4365934b1866
porovoz <falkon1@list.ru>
2022-11-15 11:04:33 +0300

    homework 10 done. Please check!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         // homework 10 started
+        // task 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("ФИО сотрудника - " + lastName + " " + firstName + " " + middleName);
 
+        // task 2
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName.toUpperCase());
+
+        // task 3
+        String fullName1 = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника - " + fullName1.replace('ё', 'е'));
     }
 }
